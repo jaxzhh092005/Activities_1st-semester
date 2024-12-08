8b9b23ee63a49e789c5f74f573b938d4961ebe4e
Jazmin Cabasa Caña <canajaja0920@gmail.com>
2024-12-08 20:26:47 +0800

    Create Activity3.py

---
+++ Activity3.py
@@ -0,0 +1,9 @@
+#string formatting
+#biodata program
+#to concatenate 3 inputs to form a singular print
+
+name = input("Enter your name: ")
+age = eval(input("Enter your age: "))
+address = input("Enter your address: ")
+
+print(f"\nHi {name}, from {address} and currently {age} years old")
